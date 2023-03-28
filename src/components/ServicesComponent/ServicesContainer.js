0c5d09fc1800c1d37c27b687e6a6afe0fa4862f2
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-03-27 18:21:52 -0700

    set up image div with the different colored rectangles

--- src/components/ServicesComponent/ServicesContainer.js
+++ src/components/ServicesComponent/ServicesContainer.js
@@ -1,23 +1,26 @@
 import Services from "./Services";
 import { services } from "../../helpers/services";
 import Box from "@mui/material/Box";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const ServicesContainer = () => {
   return (
     <Box
       sx={{
         display: "flex",
+        flexDirection: "column-reverse",
         width: "85%",
         mx: "auto",
         my: "2rem",
+        // border: "3px solid yellow",
       }}
     >
       <Box
         sx={{
           display: "flex",
-          width: "50%",
+          // width: "50%",
           justifyContent: "space-between",
-          border: "3px solid red",
+          // border: "3px solid red",
         }}
       >
         <Box>
@@ -28,10 +31,40 @@ const ServicesContainer = () => {
       </Box>
       <Box
         sx={{
-          width: "50%",
+          // border: "3px solid darkblue",
+          display: "flex",
+          flexDirection: "row-reverse",
+          height: "55vh",
         }}
       >
-        image
+        <Box
+          sx={{
+            // border: "3px solid green",
+            minWidth: "30%",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "darkgray ",
+              height: "50%",
+            }}
+          ></Box>
+          <Box
+            sx={{
+              backgroundColor: "papayaWhip ",
+              height: "50%",
+            }}
+          ></Box>
+        </Box>
+
+        <Box sx={{ display: "flex" }}>
+          <ImageListItem sx={{ objectFit: "fit" }}>
+            <img
+              src="https://live.staticflickr.com/65535/52775451167_75d9479a92_c.jpg"
+              alt=""
+            />
+          </ImageListItem>
+        </Box>
       </Box>
     </Box>
   );
